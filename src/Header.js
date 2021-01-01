f7ec598e92e9fb2de7398860bd0602c66e7cf544
vistritpandey <vistritpandey@gmail.com>
2021-01-01 17:06:24 +0530

    k

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./Header.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import { IconButton } from "@material-ui/core";
 
 function Header() {
   return (
     <div className="header">
-      <div className="header__left"></div>
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://logos-world.net/wp-content/uploads/2020/11/Gmail-Logo.png"
+          alt=""
+        />
+      </div>
 
       <div className="header__middle"></div>
 
