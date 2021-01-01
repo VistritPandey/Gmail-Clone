f5f23d0c24ac647b508635f505176798b5ff863c
vistritpandey <vistritpandey@gmail.com>
2021-01-01 18:10:05 +0530

    k

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,6 +2,9 @@ import { Button } from "@material-ui/core";
 import React from "react";
 import "./Sidebar.css";
 import AddIcon from "@material-ui/icons/Add";
+import SidebarOption from "./SidebarOption";
+import InboxIcon from "@material-ui/icons/Inbox";
+import StarIcon from "@material-ui/icons/Star";
 
 function Sidebar() {
   return (
@@ -9,6 +12,13 @@ function Sidebar() {
       <Button className="compose" startIcon={<AddIcon fontSize="large" />}>
         Compose
       </Button>
+      <SidebarOption
+        Icon={InboxIcon}
+        title="Inbox"
+        number={23}
+        selected={true}
+      />
+      <SidebarOption Icon={StarIcon} title="Starred" />
     </div>
   );
 }
