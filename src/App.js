90181ee1034e357e19acee0b0350e84fd668ba2c
vistritpandey <vistritpandey@gmail.com>
2021-01-01 10:01:05 +0530

    Material UI Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>Test</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left"></div>
+
+      <div className="header__middle"></div>
+
+      <div className="header__right"></div>
+    </div>
+  );
+}
+
+export default Header;
