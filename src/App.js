db74603ab400e6845692f5124b4ef6e9e19a2684
vistritpandey <vistritpandey@gmail.com>
2021-01-06 15:23:31 +0530

    react router added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
