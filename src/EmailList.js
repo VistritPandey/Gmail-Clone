3e582a7ab3cfd902c7e378c630510c28292e166e
vistritpandey <vistritpandey@gmail.com>
2021-01-06 15:46:52 +0530

    k

--- src/EmailList.js
+++ src/EmailList.js
@@ -4,6 +4,10 @@ import "./EmailList.css";
 import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 import RedoIcon from "@material-ui/icons/Redo";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import KeyboardHideIcon from "@material-ui/icons/KeyboardHide";
+import SettingsIcon from "@material-ui/icons/Settings";
 
 function EmailList() {
   return (
@@ -21,6 +25,23 @@ function EmailList() {
             <MoreVertIcon />
           </IconButton>
         </div>
+        <div className="emailList__settingsRight">
+          <IconButton>
+            <ChevronLeftIcon />
+          </IconButton>
+          <IconButton>
+            <ChevronRightIcon />
+          </IconButton>
+          <IconButton>
+            <KeyboardHideIcon />
+          </IconButton>
+          <IconButton>
+            <SettingsIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="emailList__sections">
+        <h2>This is the email section</h2>
       </div>
     </div>
   );
