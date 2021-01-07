822ddcb356dcfa68cd6c3232cf071bf36c5f4773
vistritpandey <vistritpandey@gmail.com>
2021-01-07 18:06:22 +0530

    k

--- src/EmailList.js
+++ src/EmailList.js
@@ -12,6 +12,7 @@ import InboxIcon from "@material-ui/icons/Inbox";
 import PeopleIcon from "@material-ui/icons/People";
 import LocalOfferIcon from "@material-ui/icons/LocalOffer";
 import Section from "./Section";
+import EmailRow from "./EmailRow";
 
 function EmailList() {
   return (
@@ -48,6 +49,9 @@ function EmailList() {
         <Section Icon={InboxIcon} title="Primary" color="red" selected />
         <Section Icon={PeopleIcon} title="Social" color="#1A73E8" />
         <Section Icon={LocalOfferIcon} title="Promotions" color="green" />
+      </div>
+      <div className="emailList__list">
+        <EmailRow />
       </div>
     </div>
   );
---
+++ src/EmailRow.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./EmailRow.css";
+
+function EmailRow() {
+  return (
+    <div className="emailrow">
+      <h1>Email goes here</h1>
+    </div>
+  );
+}
+
+export default EmailRow;
