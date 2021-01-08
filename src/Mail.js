79246822a391cd05ff7ed8adf5a64a7c12280367
vistritpandey <vistritpandey@gmail.com>
2021-01-08 07:05:15 +0530

    k

--- src/Mail.js
+++ src/Mail.js
@@ -1,10 +1,52 @@
+import { IconButton } from "@material-ui/core";
+import {
+  ArrowBack,
+  CheckCircle,
+  Delete,
+  Email,
+  Error,
+  LabelImportant,
+  MoreVert,
+  MoveToInbox,
+  WatchLater,
+} from "@material-ui/icons";
 import React from "react";
 import "./Mail.css";
 
 function Mail() {
   return (
     <div className="mail">
-      <h1>Hello</h1>
+      <div className="mail__tools">
+        <div className="mail__toolsLeft">
+          <IconButton>
+            <ArrowBack />
+          </IconButton>
+          <IconButton>
+            <MoveToInbox />
+          </IconButton>
+          <IconButton>
+            <Error />
+          </IconButton>
+          <IconButton>
+            <Delete />
+          </IconButton>
+          <IconButton>
+            <Email />
+          </IconButton>
+          <IconButton>
+            <WatchLater />
+          </IconButton>
+          <IconButton>
+            <CheckCircle />
+          </IconButton>
+          <IconButton>
+            <LabelImportant />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
