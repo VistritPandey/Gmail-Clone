01f4eac8c461926af75792658079fec3d6f20e0c
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2021-02-01 08:14:52 -0500

    Config settings removed

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCJNTbJwsAwrDpANkLv4pDWHm-IoF4S1Fg",
-  authDomain: "vistrit-email.firebaseapp.com",
-  projectId: "vistrit-email",
-  storageBucket: "vistrit-email.appspot.com",
-  messagingSenderId: "112317887339",
-  appId: "1:112317887339:web:d2f805f19c70aa9269d22e",
-  measurementId: "G-52JWK6B22H",
+  // add your own config file
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
